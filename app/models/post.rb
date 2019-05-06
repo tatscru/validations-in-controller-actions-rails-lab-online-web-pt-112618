2ea08809dbcdcc5e492bbd0c6537e404da795d7a
tatiana.mojica <tatiana.mojica@bustle.com>
2019-05-06 03:52:56 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :content, length: { minimum: 100}
-  validates :category, presence
+  validates :category,inclusion: {in: ["Fiction", "Non-Fiction"]}
 end
